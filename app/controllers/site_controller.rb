12f410d8c84432c5806115337003604aced0c036
David Woolf <davidjohnwoolf@gmail.com>
2014-08-11 13:08:49 -0600

    add contact action to site controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,5 +2,12 @@ class SiteController < ApplicationController
 
   def index
   end
-  
+
+  def contact
+    if request.post?
+      ContactMailer.contact_mailer(params).deliver
+      redirect_to root_path
+    end
+  end
+
 end
