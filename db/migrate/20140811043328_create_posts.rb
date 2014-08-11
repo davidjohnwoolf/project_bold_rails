2dd4ed8645d55d57414d279056b056315609688a
David Woolf <davidjohnwoolf@gmail.com>
2014-08-10 23:08:37 -0600

    add attributes to post migration file

--- db/migrate/20140811043328_create_posts.rb
+++ db/migrate/20140811043328_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.string :title
+      t.string :author
+      t.text :text
+      t.string :image
       t.timestamps
     end
   end
