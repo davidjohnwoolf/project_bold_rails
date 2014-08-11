bcbc10e24729f58bcf594589e3213d5fdbe911c1
David Woolf <davidjohnwoolf@gmail.com>
2014-08-11 12:45:36 -0600

    add routes for other site pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :posts
 
   root 'site#index'
+  get '/portfolio' => 'site#portfolio'
+  get '/about' => 'site#about'
+  get '/contact' => 'site#contact'
 
 end
