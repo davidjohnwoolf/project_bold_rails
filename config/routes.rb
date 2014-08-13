c07161bd24901eafbb3a1de14a28e929b96cd647
David Woolf <davidjohnwoolf@gmail.com>
2014-08-12 18:53:48 -0600

    add route for site services

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :posts
 
   root 'site#index'
+  get '/services' => 'site#services'
   get '/portfolio' => 'site#portfolio'
   get '/about' => 'site#about'
   get '/contact' => 'site#contact'
